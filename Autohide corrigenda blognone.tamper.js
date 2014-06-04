8cbcfd8072cb4aa42fb679d2579318fdc16b708b
sidisrikk <sidi.srikk@gmail.com>
2014-06-05 01:49:02 +0700

    test
    .ver 1 notification when open blognone

---
+++ Autohide corrigenda blognone.tamper.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Autohide corrigenda blognone
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      *://www.blognone.com/*
+// @copyright  2012+, You
+// ==/UserScript==
+
+alert('This is a 3rd party extension_ \n    Auto hide corrigenda comment exclusive for Blognone');
+
+
