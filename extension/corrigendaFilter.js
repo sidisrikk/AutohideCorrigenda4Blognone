1a227494e006df97465606d837fdee3394787b70
sidisrikk <sidi.srikk@gmail.com>
2014-06-06 00:57:34 +0700

    tg
    fix  conflic  toggle  v1

--- extension/corrigendaFilter.js
+++ extension/corrigendaFilter.js
@@ -3,8 +3,7 @@ $('[id^="cid"]').each(function () {
     var x = this.id.slice(4, this.id.length);
     if (parseInt(x) % 2 == 0) {
         //if meet commitment
-        $(".comment-body",this).toggle();
+        $(".comment-body", this).toggle();
+        $(this).next(".indented").toggle();
     }
 });
-
-
