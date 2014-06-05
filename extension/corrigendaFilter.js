b49cdddf27d50e56b50732cce611a3d3d798622d
sidisrikk <sidi.srikk@gmail.com>
2014-06-06 01:24:34 +0700

    lvl0
    apply only  comment   deep lvl0

--- extension/corrigendaFilter.js
+++ extension/corrigendaFilter.js
@@ -1,5 +1,5 @@
 //such a filter
-$('[id^="cid"]').each(function () {
+$("#comment-area").children('[id^="cid"]').each(function () {
     var x = this.id.slice(4, this.id.length);
     if (parseInt(x) % 2 == 0) {
         //if meet commitment
