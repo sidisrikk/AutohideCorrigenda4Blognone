7590ad0e63b8710b421e3143c675fb4e320b1953
sidisrikk <sidi.srikk@gmail.com>
2014-06-06 00:19:29 +0700

    sat
    added : saturad 100 perbanner

--- extension/corrigendaFilter.js
+++ extension/corrigendaFilter.js
@@ -1,6 +1,10 @@
+//such a filter
 $('[id^="cid"]').each(function () {
     var x = this.id.slice(4, this.id.length);
     if (parseInt(x) % 2 == 0) {
+        //if meet commitment
         $(".comment-body",this).toggle();
     }
 });
+
+
---
+++ extension/sat.js
@@ -0,0 +1,10 @@
+//  fill color on banner
+var filterVal = 'saturate(100%)';
+$('div#header')
+  .css('filter',filterVal)
+  .css('webkitFilter',filterVal)
+  .css('mozFilter',filterVal)
+  .css('oFilter',filterVal)
+  .css('msFilter',filterVal);
+
+//"web_accessible_resources": ["onclickIcon_contentscript.js"],
